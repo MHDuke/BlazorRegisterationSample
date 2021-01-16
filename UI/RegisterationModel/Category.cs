95ab9ee597c9a3489a7bfb50e4d961b19d669c6b
mohammad hani <mohammed.h.sabry@gmail.com>
2021-01-16 12:41:29 +0200

    register

---
+++ UI/RegisterationModel/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace UI.RegisterationModel
+{
+    public class Category
+    {
+        //"Fitness club", "hair dresser", "spa"
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public static IEnumerable<Category> Categories => new[]
+        {
+            new Category { Id=1, Name = "Fitness club" },
+            new Category { Id = 2, Name = "hair dresser"},
+            new Category { Id=3, Name =  "spa"}
+        };
+    }
+}
---
+++ UI/RegisterationModel/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.RegisterationModel
+{
+    public class Profile
+    {
+        public string BusinessName { get; set; }
+        public string OwnerName { get; set; }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
